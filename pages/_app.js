c6a1b708e74ba3f8a7bbe26b3c8b1c2d84937b19
Leonardo Petrillii <jxhcbhudgfv@gmail.com>
2025-03-24 09:42:32 -0300

    Analytics Vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import '../styles/styles.css';
+import { Analytics } from '@vercel/analytics/next';
 
 function MyApp({ Component, pageProps }) {
     return (
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps }) {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>
             <Component {...pageProps} />
+            <Analytics />
         </>
     );
 }
